b0201f60acdd4d314ac828e0ba8bd9a1c6455614
Ismael Isuani <ismaelisuani@gmail.com>
2014-03-06 23:37:49 -0300

    Add payment expired

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -4,4 +4,8 @@ class Payment < ActiveRecord::Base
   default_scope -> { order("#{table_name}.number ASC") }
 
   belongs_to :loan
+
+  def expired?
+    expired_at.end_of_day.past? && !paid_at
+  end
 end
--- test/models/payment_test.rb
+++ test/models/payment_test.rb
@@ -6,11 +6,22 @@ class PaymentTest < ActiveSupport::TestCase
   end
 
   test 'blank attributes' do
-    @payment = Payment.new(number: '')
+    payment = Payment.new(number: '')
+
+    assert payment.invalid?
+    assert_error payment, :number, :blank
+    assert_error payment, :expired_at, :blank
+    assert_error payment, :loan, :blank
+
+    @payment.update(paid_at: nil)
 
     assert @payment.invalid?
-    assert_error @payment, :number, :blank
-    assert_error @payment, :expired_at, :blank
-    assert_error @payment, :loan, :blank
+    assert_error @payment, :paid_at, :blank
+  end
+
+  test 'payment expired' do
+    @payment.update(expired_at: 1.day.ago, paid_at: nil)
+
+    assert @payment.expired?
   end
 end
