38105af3d9bf7a93435667921a8c89c66ca1bd60
Ismael Isuani <ismaelisuani@gmail.com>
2014-03-12 11:44:55 -0300

    Fix before_save on payments

--- app/models/concerns/payments/loan_update.rb
+++ app/models/concerns/payments/loan_update.rb
@@ -2,12 +2,13 @@ module Payments::LoanUpdate
   extend ActiveSupport::Concern
 
   included do
-    before_save :update_loan_status, on: :update
+    before_save :update_loan_status, if: ->(payment) { payment.persisted? }
   end
 
   private
     def update_loan_status
       loan.progress = percentage_progress
+      loan.next_payment_expire_at = next_payment_expiration
       loan.canceled_at = payments_dates.max if canceled?
       loan.status = 'canceled' if canceled?
     end
@@ -30,5 +31,9 @@ module Payments::LoanUpdate
 
     def canceled?
       @_canceled ||= (paid_payments_count == loan.payments_count)
+    end
+
+    def next_payment_expiration
+      self_and_siblings.detect { |payment| payment.paid_at.nil? }.expired_at
     end
 end
