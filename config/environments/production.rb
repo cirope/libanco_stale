888ef5adc2e1f2ca6bc54fbb58dd9b2e6caa6112
Franco Catena <francocatena@gmail.com>
2014-03-13 08:48:59 -0300

    Add png to the precompile path in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,7 +59,7 @@ Finance::Application.configure do
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-  config.assets.precompile += %w( *.svg *.eot *.woff *.ttf )
+  config.assets.precompile += %w( *.svg *.eot *.woff *.ttf *.png )
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
