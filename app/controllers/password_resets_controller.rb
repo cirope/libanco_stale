a3a6ae70e894cec78406a61fe9462825dbb51487
Franco Catena <francocatena@gmail.com>
2014-04-14 16:22:29 -0300

    Use sidekiq in password resets

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -10,7 +10,7 @@ class PasswordResetsController < ApplicationController
 
     if user
       user.prepare_password_reset
-      UserMailer.password_reset(user).deliver
+      UserMailer.delay.password_reset user
 
       redirect_to root_url, notice: t('.notice', scope: :flash)
     else
