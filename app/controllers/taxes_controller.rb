9c5096a15363f223a7c32f3a45037efc9761874f
Ismael Isuani <ismaelisuani@gmail.com>
2014-04-22 11:11:04 -0300

    End less taxes

--- app/controllers/taxes_controller.rb
+++ app/controllers/taxes_controller.rb
@@ -2,11 +2,12 @@ class TaxesController < ApplicationController
   include Authorization
   include Taxes::SearchScoped
 
-  respond_to :html, :json
+  respond_to :html, :json, :js
 
   before_action :set_title
 
   # GET /tax_settings/1/taxes
   def index
+    @taxes = @taxes.page params[:page]
   end
 end
