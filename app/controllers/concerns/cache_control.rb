3e1bf94728fda0840cddac2da9d585c2b47f5fb9
Ismael Isuani <ismaelisuani@gmail.com>
2014-05-05 21:57:20 -0300

    Improve cache control

--- app/controllers/concerns/cache_control.rb
+++ app/controllers/concerns/cache_control.rb
@@ -2,6 +2,15 @@ module CacheControl
   extend ActiveSupport::Concern
 
   included do
-    after_action -> { expires_now if current_user }
+    after_filter :set_cache_control
   end
+
+  private
+    def set_cache_control
+      if current_user
+        response.headers['Cache-Control'] = 'no-cache, no-store, max-age=0, must-revalidate'
+        response.headers['Pragma'] = 'no-cache'
+        response.headers['Expires'] = 'Fri, 01 Jan 1990 00:00:00 GMT'
+      end
+    end
 end
